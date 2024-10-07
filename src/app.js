76e9ae1d2800f4df3481f03df21704819776c17f
Zaid Shaikh <s.zaidshaikh99@gmail.com>
2024-10-07 23:35:31 +0530

    Exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the landing page");
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the landing page");
+// });
+
+// app.get("/test", (req, res) => {
+//   res.send("Testing page!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("Hello everyone!");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Zaid", lastName: "Shaikh" });
 });
 
-app.get("/test", (req, res) => {
-  res.send("Testing page!");
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data successfully saved to the database");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello everyone!");
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully!");
 });
 
 app.listen(3000, () => {
